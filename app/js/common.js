7c79eed4b2d4f6172baf6bd7c1db157aee06e021
gunaevart <gunaevart@mail.ru>
2018-06-05 12:01:05 +0300

    Делаю форму заказа

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,25 @@
-$(function() {
 
-	// Пользовательские функции
+// Masked phone
+$("#icon_telephone").mask("(999) 999-9999");
 
+// scroll top
+
+	$('.contacts__btn').click(function(){
+
+		 $('html, body').animate({scrollTop:0}, 'slow');
+ });
+
+ // scroll catalog
+ $('.catalog').click(function(){
+		
+	$('html, body').animate({scrollTop:3200}, 'slow');
 });
+
+//callback
+
+document.querySelector('#callback').onclick = function(){
+
+	let	phone = document.querySelector('#icon_telephone').value
+
+//	console.log(phone);
+}
